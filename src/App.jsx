171692a1b13a9be1d5f002ff9144e472e519a2c5
Arsen <arsen.aghavelyan.ait@gmail.com>
2023-09-03 17:21:45 +0300

    change the pagination of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
   // console.log(news);
 
   const [page, setPage] = useState(1)
+  const [pageCount, setPageCount] = useState(0)
   useEffect(() => {
     fetch(`${NewsAPI_URL}?country=${country}&category=${category}&pageSize=10&page=${page}&apiKey=${API_KEY}`).then((response) => {
       return response.json()
@@ -25,20 +26,13 @@ function App() {
       dispatch(handleNews({
         res: res.articles
       }))
+      setPageCount(res.totalResults)
     })
   }, [category, country, page])
 
-  function updatePage(pageNumber) {
-    setPage(pageNumber)
-  }
-
-  function handleNextPage() {
-    updatePage(page + 1)
-  }
-
-  function handlePrevPage() {
-    updatePage(page - 1)
-  }
+  function handlePageChange(newPage) {
+    setPage(newPage)
+}
   return (
     <>
       <div className='bg-gray-600'>
@@ -52,19 +46,20 @@ function App() {
             })
           }
         </div>
-        <div className='flex gap-2 justify-center'>
-          <div className={`w-[30px] h-[30px]  border-white border-[1px] rounded-[50%]  flex justify-center items-center p-2 cursor-pointer mt-5 ${page === 1 ? 'opacity-50 pointer-events-none' : ''}`} onClick={handlePrevPage}>
-            <span>&lt;</span>
-          </div>
-          <div className={`${page == 1 ? "bg-white" : "bg-transparent"} w-[30px] h-[30px]  border-white border-[1px] rounded-[50%]  flex justify-center items-center p-2 cursor-pointer mt-5`} onClick={() => updatePage(1)}>
-            <span>1</span>
-          </div>
-          <div className={` ${page == 2 ? "bg-white" : "bg-transparent"} w-[30px] h-[30px]  border-white border-[1px] rounded-[50%]  flex justify-center items-center p-2 mb-3 mt-5 cursor-pointer`} onClick={() => updatePage(2)}>
-            <span>2</span>
-          </div>
-          <div className={`w-[30px] h-[30px]  border-white border-[1px] rounded-[50%]  flex justify-center items-center p-2 cursor-pointer mt-5 ${news.length === 0 ? 'opacity-50 pointer-events-none' : ''}`} onClick={handleNextPage}>
-            <span>&gt;</span>
-          </div>
+        <div className='bg-gray-600 flex gap-4 p-5 justify-center'>
+          <button
+            className={`${page > 1 ? "w-[35px] h-[35px] bg-white border-white border-[1px] rounded-[50%]  flex justify-center items-center p-2 cursor-pointer mt-5" : "opacity-50 pointer-events-none w-[35px] h-[35px]  border-white border-[1px] rounded-[50%]  flex justify-center items-center p-2 cursor-pointer mt-5"} `}
+            onClick={() => handlePageChange(page - 1)}
+          >
+            &lt;
+          </button>
+          
+          <button
+            className={`${page * 20 < pageCount && page * 20 < 100 ? "w-[35px] h-[35px] bg-white border-white border-[1px] rounded-[50%]  flex justify-center items-center p-2 cursor-pointer mt-5" : "opacity-50 pointer-events-none w-[35px] h-[35px]  border-white border-[1px] rounded-[50%]  flex justify-center items-center p-2 cursor-pointer mt-5"} `}
+            onClick={() => handlePageChange(page + 1)}
+          >
+            &gt;
+          </button>
         </div>
       </div>
     </>
